e69eb7a6b76ed2c024168d9efbc55abb5dc51dc4
shateeshb <bhugwansing.shateesh@gmail.com>
2018-12-03 17:44:44 -0500

    working on transaction still

--- frontend/src/components/transaction-data/transaction.jsx
+++ frontend/src/components/transaction-data/transaction.jsx
@@ -8,25 +8,39 @@ class Transaction extends Component {
     constructor(props){
         super(props); 
         this.state = {
-            data : '',
+            data : [],
+            test: 'initial Data'
         }
     }
     componentDidMount() {
-      fetch('/plaid/transactions')
-        .then(body => console.log('Is there a body? ', (body!=null)));
+    const instance = axios.create({
+        baseURL : 'http://localhost:8000/plaid/',
+    });
+
+    instance.post('/get_access_token')
+    .catch(error => {
+        console.log(error); 
+    }); 
+        //   axios.get('http://localhost:8000/plaid/transactions')
+    //     .then(body => {
+    //         console.log('Is there a body? ');
+    //         console.log(body);
+    //     }).catch(error => {
+    //         console.log(error); 
+    //     });
     }
 
-    getTransactionData = () => {
-        axios.get('/plaid').then(res => {
-            let transactions = res.data; 
-            this.setState({data : transactions}); 
-        });
-    }
+    // getTransactionData = () => {
+    //     axios.get('/plaid/transactions').then(res => {
+    //         let transactions = res.data; 
+    //         this.setState({data : transactions}); 
+    //     });
+    // }
   
     render() {
       return (
         <div>
-            {this.state.data}
+            data: {this.state.data}
         </div>
       );
     }
