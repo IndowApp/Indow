4a713373507c6d0c750cb838870f6af689a13dba
Julia Helena <jh@juliahelena.me>
2018-12-02 21:38:23 -0500

    general front-page styles and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,41 @@ import './App.css';
 import Goals from './components/goals-chart/goals-chart';
 import SideBar from './components/side-bar/side-bar';
 import MarketMover from './components/market-mover/market-mover';
+//import Sidebar from "react-sidebar";
 
 class App extends Component {
+  // the code that is commented out is for the collapsible side bar we can use when we work on making the app responsive
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     sidebarOpen: true
+  //   };
+  //   this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  // }
+  // onSetSidebarOpen(open) {
+  //   this.setState({ sidebarOpen: open });
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        {/* <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "white" } }}
+      >
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Open sidebar
+        </button>
+        </Sidebar> */}
           <SideBar />
-          <Goals />
-          <MarketMover />
+          <div className="mainBody">
+            <Goals />
+            <MarketMover />
+          </div>
         </header>
       </div>
     );
--- frontend/src/components/goals-chart/goals-chart.jsx
+++ frontend/src/components/goals-chart/goals-chart.jsx
@@ -6,24 +6,25 @@ import './goals.css'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
-    {name: 'November 2018', uv: 4000, pv: 2400, amt: 2400},
-    {name: 'Drinks & Food', uv: 3000, pv: 1398, amt: 2210},
-    {name: 'Groceries', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Transportation', uv: 2780, pv: 3908, amt: 2000},
-    {name: 'Invested', uv: 1890, pv: 4800, amt: 2181},
+    {name: 'November 2018', pv: 50, uv:350},
+    {name: 'Drinks & Food', pv: 150, uv:125},
+    {name: 'Groceries', pv: 250, uv:52},
+    {name: 'Transportation', pv: 350, uv:20},
+    {name: 'Invested', pv: 450, uv:100},
+    
 ];
 class Goals extends Component {
 	render () {
   	return (
         <div className="goals">
           <h3>Your Goals</h3>
-    	    <BarChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}} layout="vertical" className="goalsChart">
-            <CartesianGrid strokeDasharray="3 3"/>
-            <XAxis />
-            <YAxis dataKey="name" type="category"/>
+    	    <BarChart width={650} height={300} data={data} margin={{top: 5, right: 30, left: 50, bottom: 5}} layout="vertical" className="goalsChart">
+            <CartesianGrid />
+            <XAxis tick={{ fill: '#282c34' }} />
+            <YAxis dataKey="name" type="category" tick={{ fill: '#282c34' }}/>
             <Tooltip/>
             <Legend />
-            <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+            <Bar dataKey="pv" fill="#62d2a2" background={{ fill: 'white' }} />
           </BarChart>
         </div>
     );
