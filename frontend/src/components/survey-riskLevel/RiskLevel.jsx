f1eb571e84a9833f45da5790c816c81c33ae38d1
Julia Helena <jh@juliahelena.me>
2018-12-09 18:09:10 -0500

    survey interest areas question final

---
+++ frontend/src/components/survey-riskLevel/RiskLevel.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './RiskLevel.css';
+
+class RiskLevel extends Component {
+    render () { 
+        return (
+            <div className="riskLevel-wrapper"></div>
+        );
+    }
+}
+export default RiskLevel;
--- frontend/src/pages/survey/survey.jsx
+++ frontend/src/pages/survey/survey.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './survey.css';
 import InterestArea from '../../components/survey-interestArea/InterestArea';
+import RiskLevel from '../../components/survey-riskLevel/RiskLevel'
 
 class Survey extends Component {
     render () {
         return (
             <div className="surveyWrapper">
+            <link href="https://fonts.googleapis.com/css?family=Mukta:300,600" rel="stylesheet" />
                <InterestArea />
+               <RiskLevel />
             </div>
         );
     }
