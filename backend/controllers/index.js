e5e2d34169cce6953d2f0db1f0ec56d7625229dd
shateeshb <bhugwansing.shateesh@gmail.com>
2018-12-09 18:57:20 -0500

    fixed start scripts

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/plaid', require('./plaidController'));
-router.use('/', require('./home'));
+// router.use('/', require('./home'));
 
 module.exports = router;
