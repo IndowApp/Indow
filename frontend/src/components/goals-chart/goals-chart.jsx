d3bd3254758dd0d2143e8032f22af850785501fc
shateeshb <bhugwansing.shateesh@gmail.com>
2018-12-12 18:16:42 -0500

    editing interests

--- frontend/src/components/goals-chart/goals-chart.jsx
+++ frontend/src/components/goals-chart/goals-chart.jsx
@@ -6,26 +6,29 @@ import './goals.css'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
-    {name: 'November 2018', pv: 150},
-    {name: 'Drinks & Food', pv: 160},
-    {name: 'Groceries', pv: 250},
-    {name: 'Transportation', pv: 350},
-    {name: 'Invested', pv: 450},
-    
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ];
 class Goals extends Component {
 	render () {
   	return (
         <div className="goals">
           <h3>Your Goals</h3>
-    	    <BarChart width={650} height={300} data={data} margin={{top: 5, right: 30, left: 50, bottom: 5}} layout="vertical" className="goalsChart">
-            <CartesianGrid />
-            <XAxis tick={{ fill: '#282c34' }} />
-            <YAxis dataKey="name" type="category" tick={{ fill: '#282c34' }}/>
+          <BarChart width={800} height={400} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="name"/>
+            <YAxis/>
             <Tooltip/>
             <Legend />
-            <Bar dataKey="pv" fill="#62d2a2" background={{ fill: 'white' }} />
-          </BarChart>
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
         </div>
     );
   }
